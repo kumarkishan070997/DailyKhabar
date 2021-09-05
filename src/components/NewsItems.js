075e8c8bf458cb07d800ef06977ec5a552358749
kishan kumar maharana <kishan.maharana@mail.vinove.com>
2021-09-05 11:49:45 +0530

    dynamic render of news

--- src/components/NewsItems.js
+++ src/components/NewsItems.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react'
 
 export class NewsItems extends Component {
+    dummyUrlImage = "https://previews.123rf.com/images/leodelagarza/leodelagarza2005/leodelagarza200500034/147045749-dummy-online-dummy-news-website-sketch-copy-space.jpg";
     render() {
         return (
+            <>
             <div>
-                this is news item
+            <div className="card" style={{"width": "18rem"}}>
+                <img src={this.props.image ? this.props.image : this.dummyUrlImage} className="card-img-top" alt="image" />
+                <div className="card-body">
+                    <h5 className="card-title">{this.props.title}</h5>
+                    <p className="card-text">{this.props.description}</p>
+                    <a href="/" className="btn btn-primary">Read More</a>
+                </div>
+                </div>
             </div>
+            </>
         )
     }
 }
