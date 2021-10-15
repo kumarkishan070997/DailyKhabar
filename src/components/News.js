2bd098a00ec75f93989a7c13ed408b219f8caad0
kishan kumar maharana <kishan.maharana@mail.vinove.com>
2021-10-15 14:21:13 +0530

    listing issues fix

--- src/components/News.js
+++ src/components/News.js
@@ -4,18 +4,19 @@ import NewsItems from './NewsItems'
 export class News extends Component {
     constructor(){
         super();
-        this.state = ({
-            articles : [],
-            loading : false
-        })
+        this.state = {
+            articles: [],
+            loading: false
+        }
     }
     async componentDidMount(){
         let apiUrl = "https://newsapi.org/v2/everything?q=Apple&sortBy=popularity&apiKey=7844984c37a0488e874a07df1c84543a";
         let data = await fetch(apiUrl);
-        let parseData = await data.json();
-        // console.log(parseData);
-        this.state = ({
-            articles:parseData.articles
+        let parseData =  await data.json();
+        console.log(parseData.articles);
+        this.setState({
+            articles:parseData.articles,
+            loading: true
         })
         console.log(this.state.articles);
     }
@@ -25,7 +26,6 @@ export class News extends Component {
                 <h2>Top Trendings</h2>
                 <div className="row">
                     {this.state.articles.map((element)=>{
-                        console.log(element);
                         return <div className="col-md-4" key={element.url}>
                         <NewsItems title={element.title} description={element.description} image={element.urlToImage} readLink={element.url}/>
                     </div>
